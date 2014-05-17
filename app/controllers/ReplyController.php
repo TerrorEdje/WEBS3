548b6ffcc8eaace2c7dddb1fa664d337d6c31dd4
Jip <jip--pie@hotmail.com>
2014-05-17 17:34:31 +0200

    Query toegevoegd aan de replyController. Een reactie wordt nu in de database gezet.

--- app/controllers/ReplyController.php
+++ app/controllers/ReplyController.php
@@ -4,25 +4,19 @@ class ReplyController extends BaseController {
 
 	public function addReply($id)
 	{		
+		$reply = new Reply;
+		$reply->content = Input::get('content');
+		$reply->date = date("Y-m-d H:i:s");
+		$reply->by = Input::get('username'); # Je moet in het formulier het id van de user invullen
+		$reply->topics_id = $id;
+		$reply->save();
+		
 		echo "REACTIE IS TOEGEVOEGD !!!";
 		
 		$allInfoTopic = $this->getTopicInfo($id);
 		
 		$infoTopic = $allInfoTopic['infoTopic'];
 		$infoReplies = $allInfoTopic['infoReplies'];
-		
-		/*$infoTopic = array();
-		$infoTopic['topic'] = Topic::find($id);
-		$infoTopic['by'] = User::find($infoTopic['topic']->by);
-		
-		$infoReplies = array();
-		$dbReplies = Reply::where('topics_id', '=', $id)->get();
-		foreach ($dbReplies as $reply) {
-			$infoReply = array();
-			$infoReply['reply'] = $reply;
-			$infoReply['by'] = User::find($reply->by);
-			array_push($infoReplies, $infoReply);
-		}*/
 		
 		return View::make('topic')->with('topic', $infoTopic)->with('replies', $infoReplies);
 	}
