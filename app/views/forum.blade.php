a91bc1375e3591788170a45a68d39a98d3d00aaf
Jip <jip--pie@hotmail.com>
2014-05-17 23:07:33 +0200

    Piep klein beginnetje gemaakt aan de layout van forum view

--- app/views/forum.blade.php
+++ app/views/forum.blade.php
@@ -2,17 +2,35 @@
 <h1>Categories</h1>	
 
 @foreach ($categories as $infoCategory)
-	{{ $infoCategory['category']->name }}<br/>
+	
+	<div>
+	
+		<div>
+			{{ $infoCategory['category']->name }}<br/>
+		</div>
+		
+		<div>
+			<span>Topics</span>
+			<span>Replies</span>
+			<span>Last reply</span>
+		</div>
 
-	@foreach ($infoCategory['subcategories'] as $infoSubcategory)
-		{{ link_to_route('topics', $infoSubcategory['name'] , array('name' => $infoSubcategory['name'] )) }}
-		Aantal topics: {{ $infoSubcategory['amountOfTopics'] }} 
-		Aantal reacties: {{ $infoSubcategory['amountOfReplies'] }}
-		@if ($infoSubcategory['lastReply'] == 0) 
-			Laatste reactie: -<br/>
-		@else
-			Laatste reactie: {{ $infoSubcategory['lastReply'] }}<br/>
-		@endif
-	@endforeach
+		<div>
+			@foreach ($infoCategory['subcategories'] as $infoSubcategory)
+				<span> {{ link_to_route('topics', $infoSubcategory['name'] , array('name' => $infoSubcategory['name'] )) }} </span>
+				<span> {{ $infoSubcategory['amountOfTopics'] }} </span>
+				<span> {{ $infoSubcategory['amountOfReplies'] }} </span>
+				@if ($infoSubcategory['lastReply'] == 0) 
+					<span> - </span>
+				@else
+					<span> {{ $infoSubcategory['lastReply'] }} </span>
+				@endif
+				<br/>
+			@endforeach
+		</div>
+	
+	</div>
+	
 	<br/>
+	
 @endforeach
