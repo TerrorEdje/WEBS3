4010534d56fdd30badb06418ed4c8212f54d683b
Edwin Hattink <edwinhattink@me.com>
2014-05-19 17:26:05 +0200

    DB, kleine update

--- DatabaseOntwerp/create.sql
+++ DatabaseOntwerp/create.sql
@@ -37,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `rights_name` VARCHAR(45) NOT NULL,
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
+  `active` INT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
