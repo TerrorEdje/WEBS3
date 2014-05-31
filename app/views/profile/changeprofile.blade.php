41862c2c38d0d615118fc6c302f8bdf94601d1eb
Jip <jip--pie@hotmail.com>
2014-05-31 21:39:41 +0200

    Layout van Change profile

--- app/views/profile/changeprofile.blade.php
+++ app/views/profile/changeprofile.blade.php
@@ -1,24 +1,38 @@
 @extends('layout.main')
 
 @section('content')
-		<form action="{{ URL::route('profile-change-post') }}" method="post" enctype="multipart/form-data">
-		<div class="field">
-			Current image:{{ HTML::image('uploads/' . $user['image'],'Image', array('width' => '100', 'height' => '100'))}}
-		</div>
-		<div class="field">
-			Image: {{Form::file('image')}}
-			@if($errors->has('image'))
-				{{ $errors->first('image') }}
-			@endif
-		</div>
-		<div class="field">
-			Signature: {{Form::textarea('signature',$user['signature'])}}
-			@if($errors->has('signature'))
-				{{ $errors->first('signature') }}
-			@endif
-		</div>
+
+	<div class="col-md-12 titleBlock bg-primary">
+		Change profile
+	</div>
+	
+	<div class="col-md-12 form">
+		{{ Form::open(array('route' => array('profile-change-post'))) }}
 		
-		<input type="submit" value="Update profile">
-		{{ Form::token(); }}
-	</form>
+			Current image:<br>
+			{{ HTML::image('uploads/' . $user['image'],'Image', array('width' => '100', 'height' => '100')) }}<br>
+		
+			<br>
+			
+			Image:<br>
+			{{ Form::file('image') }}
+			@if($errors->has('image'))
+				{{ $errors->first('image', '<span class="text-danger">:message</span>') }}<br>
+			@endif
+			
+			<br>
+			
+			Signature:<br>
+			{{ Form::textarea('signature', $user['signature'], array('class' => 'textarea')) }}
+			@if($errors->has('signature'))
+				{{ $errors->first('signature', '<span class="text-danger">:message</span>') }}<br>
+			@endif
+		
+			<br>
+			
+			{{ Form::submit('Update profile', array('class' => 'btn-primary button')) }}
+			
+		{{ Form::token() }}	
+		{{ Form::close() }}	
+	</div>
 @stop
