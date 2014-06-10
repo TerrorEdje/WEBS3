b2b3d7fe849dc415694a35b3780c6bd8edd334c0
Jip <jip--pie@hotmail.com>
2014-06-10 20:42:54 +0200

    Klein beginnetje gemaakt aan layout permissons

--- app/views/settings/permissions.blade.php
+++ app/views/settings/permissions.blade.php
@@ -1,25 +1,41 @@
 @extends('layout/main')
 
 @section('content')
-	{{ Form::open(array('route' => array('manage-permissons-post'))) }}
-		{{ Form::hidden('amountOfUsers',count($users)) }}
-		<table>
-		<?php $index = 0; ?>
-		@foreach($users as $user)
-			<tr>
-				<td>
-					{{ $user->username }}
-					{{ Form::hidden('userid'. $index,$user->id) }}
-				</td>
-				<td>
-					{{Form::select('right' . $index,$rights, $user['rights_id']) }}
-				</td>
-			</tr>
-			<?php $index++; ?>
-		@endforeach
-		</table>
-		{{ Form::submit('Update permissions', array('class' => 'btn-primary button')) }}
+
+	<div class="col-md-12 titleBlock bg-primary">
+		Permissons
+	</div>
+	
+	<div class="col-md-12 form">
+	
+		{{ Form::open(array('route' => array('manage-permissons-post'))) }}
+			
+			{{ Form::hidden('amountOfUsers', count($users)) }}
+			<table class="col-sm-12">
+				<?php $index = 0; ?>
+				@foreach($users as $user)
+					<tr>
+						<td class="col-sm-1">
+							{{ $user->username }}
+							{{ Form::hidden('userid'. $index,$user->id) }}
+						</td>
+						<td class="col-sm-11 tableTD">
+							{{Form::select('right' . $index, $rights, $user['rights_id'], array('class' => 'selectbox')) }}
+						</td>
+					</tr>
+					<?php $index++; ?>
+				@endforeach
+			</table>
+			
+			{{ Form::submit('Update permissions', array('class' => 'btn-primary button')) }}
+			
 		{{ Form::token() }}	
-	{{ Form::close() }}	
-	<small>This list does not show yourself, to protect the site from losing all admins.</small>
+		{{ Form::close() }}	
+		
+		<br>
+		
+		<small>This list does not show yourself, to protect the site from losing all admins.</small>
+
+	</div>
+	
 @stop
