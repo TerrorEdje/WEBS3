cd3f3064fbbd351fd48e308aa1e42963591bc651
Jip <jip--pie@hotmail.com>
2014-06-11 11:37:48 +0200

    profile view update

--- app/views/profile/user.blade.php
+++ app/views/profile/user.blade.php
@@ -9,7 +9,7 @@
 	<div>
 		<table class="col-md-12 profileTable">
 			<tr>
-				<td colspan="2" class="col-md-8 tableTD">
+				<td colspan="2" class="col-md-7 tableTD">
 					<span class="profileUsername">{{ $user->username }}</span><br>
 					{{ $user->signature }}
 				</td>
@@ -17,14 +17,20 @@
 					Last time online:<br>
 					Times online:
 				</td>
-				<td class="col-md-2 tableTD">
+				<td class="col-md-3 tableTD">
 					{{ date("D d M Y", strtotime($user->lasttimeonline)), ' at ', date("H:i", strtotime($user->lasttimeonline)) }}<br> 
 					{{ $user->timesonline }}
 				</td>
 			</tr>
 			<tr>
 				<td class="col-md-2 tableTD">{{ HTML::image('uploads/' . $user->image,'Image', array('width' => '100', 'height' => '100'))}}</td>
-				<td colspan="3" class="col-md-10 tableTD">{{ nl2br($user->description) }}</td>
+				<td colspan="3" class="col-md-10 tableTD">
+					@if ($user->description != null)
+						{{ nl2br($user->description) }}
+					@else
+						There is no description.
+					@endif
+				</td>
 			</tr>
 		</table>
 	</div>
