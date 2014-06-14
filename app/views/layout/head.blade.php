50268ef95145c2a6b87b3ec538609c407b626253
Edwin Hattink <edwinhattink@me.com>
2014-06-14 14:14:52 +0200

    Mappen structuur

--- app/views/layout/head.blade.php
+++ app/views/layout/head.blade.php
@@ -1,9 +1,9 @@
 <title>Je Forum</title>
-{{HTML::style('assets/style.css')}}
-{{HTML::style('assets/bootstrap.css')}}
-{{HTML::script('assets/jquery-1.11.1.js')}}
-{{HTML::script('assets/jquery-ui-1.10.4.js')}}
-{{HTML::script('assets/javascript.js')}}
-{{HTML::script('assets/jquery.cookie.js')}}		
-{{HTML::script('assets/bootstrap.js')}}
+{{HTML::style('assets/css/style.css')}}
+{{HTML::style('assets/css/bootstrap.css')}}
+{{HTML::script('assets/js/jquery-1.11.1.js')}}
+{{HTML::script('assets/js/jquery-ui-1.10.4.js')}}
+{{HTML::script('assets/js/javascript.js')}}
+{{HTML::script('assets/js/jquery.cookie.js')}}		
+{{HTML::script('assets/js/bootstrap.js')}}
 @include('layout/favorites')
