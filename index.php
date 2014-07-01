43fa3533b3930c38811c8af3051ccf1024a9cb26
Edwin Hattink <edwinhattink@me.com>
2014-07-01 15:43:36 +0200

    no message

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-header('Location: public');
-?>
+<html>
+<head>
+<title>Sigma Draconis</title>
+</head>
+<body>
+<a href="/public">Go to main website</a>
+<p>You will be redirected in <span id="counter">5</span> second(s).</p>
+<script type="text/javascript">
+function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML)<=0) {
+        location.href = '/public/';
+    }
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(function(){ countdown(); },1000);
+</script>
+</body>
+</html>
